5cbc01d30277a017703329c7ff3d178b049dc232
AKA <acuteangel@users.noreply.github.com>
2018-12-11 04:51:01 -0800

    fixed sql files

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
-DROP DATABASE IF EXISTS `burgers_db`;
-CREATE DATABASE `burgers_db`;
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
 
-USE `burgers_db`
+USE burgers_db;
 
-CREATE TABLE `burgers` (
-  `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
-  `burger_name` VARCHAR( 255) NOT NULL,
-  `devoured` BOOLEAN NOT NULL DEFAULT 0,
+CREATE TABLE burgers (
+  id Int( 11 ) AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT false,
 
-  PRIMARY KEY ( `id` ) 
+  PRIMARY KEY ( id ) 
 );
