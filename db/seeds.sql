6cc1cfa71e7065ed254d34d67689f6b0f53620f0
acuteangel <akaquas@gmail.com>
2018-12-10 22:16:16 -0800

    set up configurations, started model

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
-INSERT INTO `burgers` (`burger_name`) 
+INSERT INTO `burgers` (`burger_name`) VALUES (Plain Burger);
+INSERT INTO `burgers` (`burger_name`) VALUES (Cheese Burger);
+INSERT INTO `burgers` (`burger_name`) VALUES (Bacon Burger);
